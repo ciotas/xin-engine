450d42db89dedfc6706ef07ab804058ec148a76e
ciotas <linzikristy@qq.com>
2021-12-24 10:11:08 +0800

    fix bug

--- resources/views/knowledge.blade.php
+++ resources/views/knowledge.blade.php
@@ -34,7 +34,7 @@
                             </div>
 
                             <div class="ml-6">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-xs">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-md">
                                 {!!$knowledge->content!!}
                             </div>
                             </div>
